f151d965f39465a40ba9a4caaef002d60ecd68be
SoumyaJoshi06 <soumyajoshi0506@gmail.com>
2026-03-26 20:58:23 +0530

    2nd Commit

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -24,10 +24,10 @@ export const Footer = () => {
           {/* Logo & Copyright */}
           <div className="text-center md:text-left">
             <a href="#" className="text-xl font-bold tracking-tight">
-              PM<span className="text-primary">.</span>
+              SJ<span className="text-primary">.</span>
             </a>
             <p className="text-sm text-muted-foreground mt-2">
-              © {currentYear} Pedro Machado. All rights reserved.
+              © {currentYear} Soumya Joshi. All rights reserved.
             </p>
           </div>
 
